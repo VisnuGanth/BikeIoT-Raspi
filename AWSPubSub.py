e42cf0dac71b5808c9577140efccc04014408385
VisnuGanth <visnuganth07@gmail.com>
2020-10-12 14:13:01 +0530

    First Commit: 	- Contains files to get data from SenseHat Emulator and send to AWS Cloud. 	- Documentation for this is present in repo "BikeIoT-PC" under the Documentation folder.

---
+++ AWSPubSub.py
@@ -0,0 +1,148 @@
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+import logging
+import time
+import argparse
+import json
+import datetime
+import os
+from sense_emu import SenseHat
+
+#Create object to get data from the SenseHAT Emulator
+hat = SenseHat()
+hat.clear()
+
+
+AllowedActions = ['both', 'publish', 'subscribe']
+
+# Custom MQTT message callback
+def customCallback(client, userdata, message):
+    print("Received a new message: ")
+    print(message.payload)
+    print("from topic: ")
+    print(message.topic)
+    print("--------------\n\n")
+
+
+# Read in command-line parameters
+parser = argparse.ArgumentParser()
+parser.add_argument("-e", "--endpoint", action="store", required=True, dest="host", help="Your AWS IoT custom endpoint")
+parser.add_argument("-r", "--rootCA", action="store", required=True, dest="rootCAPath", help="Root CA file path")
+parser.add_argument("-c", "--cert", action="store", dest="certificatePath", help="Certificate file path")
+parser.add_argument("-k", "--key", action="store", dest="privateKeyPath", help="Private key file path")
+parser.add_argument("-p", "--port", action="store", dest="port", type=int, help="Port number override")
+parser.add_argument("-w", "--websocket", action="store_true", dest="useWebsocket", default=False,
+                    help="Use MQTT over WebSocket")
+parser.add_argument("-id", "--clientId", action="store", dest="clientId", default="basicPubSub",
+                    help="Targeted client id")
+parser.add_argument("-t", "--topic", action="store", dest="topic", default="sdk/test/Python", help="Targeted topic")
+parser.add_argument("-m", "--mode", action="store", dest="mode", default="both",
+                    help="Operation modes: %s"%str(AllowedActions))
+parser.add_argument("-M", "--message", action="store", dest="message", default="Hello World!",
+                    help="Message to publish")
+
+args = parser.parse_args()
+host = args.host
+rootCAPath = args.rootCAPath
+certificatePath = args.certificatePath
+privateKeyPath = args.privateKeyPath
+port = args.port
+useWebsocket = args.useWebsocket
+clientId = args.clientId
+topic = args.topic
+
+if args.mode not in AllowedActions:
+    parser.error("Unknown --mode option %s. Must be one of %s" % (args.mode, str(AllowedActions)))
+    exit(2)
+
+if args.useWebsocket and args.certificatePath and args.privateKeyPath:
+    parser.error("X.509 cert authentication and WebSocket are mutual exclusive. Please pick one.")
+    exit(2)
+
+if not args.useWebsocket and (not args.certificatePath or not args.privateKeyPath):
+    parser.error("Missing credentials for authentication.")
+    exit(2)
+
+# Port defaults
+if args.useWebsocket and not args.port:  # When no port override for WebSocket, default to 443
+    port = 443
+if not args.useWebsocket and not args.port:  # When no port override for non-WebSocket, default to 8883
+    port = 8883
+
+# Configure logging
+logger = logging.getLogger("AWSIoTPythonSDK.core")
+logger.setLevel(logging.DEBUG)
+streamHandler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+streamHandler.setFormatter(formatter)
+logger.addHandler(streamHandler)
+
+# Init AWSIoTMQTTClient
+myAWSIoTMQTTClient = None
+if useWebsocket:
+    myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId, useWebsocket=True)
+    myAWSIoTMQTTClient.configureEndpoint(host, port)
+    myAWSIoTMQTTClient.configureCredentials(rootCAPath)
+else:
+    myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId)
+    myAWSIoTMQTTClient.configureEndpoint(host, port)
+    myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+
+# AWSIoTMQTTClient connection configuration
+myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
+myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+
+# Connect and subscribe to AWS IoT
+myAWSIoTMQTTClient.connect()
+if args.mode == 'both' or args.mode == 'subscribe':
+    myAWSIoTMQTTClient.subscribe(topic, 1, customCallback)
+time.sleep(2)
+
+#Device ID - unique to every Pi
+DEVICE_ID = 'ce51fe44-5212-7a86-e42e-e87c8eb90795'#DEVICE1
+#DEVICE_ID = '02241467-6da7-44cd-9ada-6e9f8095646f'#DEVICE2
+#DEVICE_ID = '5d52176f-9613-4ab7-aedf-386941c2eec6'#DEVICE3
+
+# Publish to the same topic in a loop forever
+while True:
+    if args.mode == 'both' or args.mode == 'publish':
+        
+        #Getting magnetometer readings
+        mag = hat.get_compass_raw()
+        mag_data = []
+        mag_data.append(round(mag["x"],2))
+        mag_data.append(round(mag["y"],2))
+        mag_data.append(round(mag["z"],2))
+        
+        #Getting accelerometer readings
+        acc = hat.get_accelerometer_raw()
+        acc_data = []
+        acc_data.append(round(acc["x"],3))
+        acc_data.append(round(acc["y"],3))
+        acc_data.append(round(acc["z"],3))
+        
+        #Getting gyroscope reading
+        gyro = hat.get_orientation()
+        gyro_data = []
+        gyro_data.append(round(gyro["pitch"],2))
+        gyro_data.append(round(gyro["yaw"],2))
+        gyro_data.append(round(gyro["roll"],2))
+        
+        #Get the current date and time to use as timestamp
+        timestamp = time.strftime("%Y-%m-%d %X", time.localtime())
+        
+        message = {}
+        message['magnetometer'] = str(mag_data)
+        message['accelerometer'] = str(acc_data)
+        message['gyroscope'] = str(gyro_data)
+        message['device_id'] = DEVICE_ID
+        message['timestamp'] = timestamp
+        
+        messageJson = json.dumps(message)
+        myAWSIoTMQTTClient.publish(topic, messageJson, 1)
+        if args.mode == 'publish':
+            print('Published topic %s: %s\n' % (topic, messageJson))
+    time.sleep(0.5)
---
+++ IMU_data.py
@@ -0,0 +1,55 @@
+from time import sleep
+from sense_emu import SenseHat
+from PIL import Image, ImageDraw
+import time
+import datetime
+import os
+
+#Create object to get data from the SenseHAT Emulator
+hat = SenseHat()
+
+sleep(5)
+hat.clear()
+
+#Get the current date and time to use as filenaame
+fname = time.strftime("%Y-%m-%d %X", time.localtime())
+path = os.path.join('/home/pi/Desktop/'+str(fname)+'.txt')
+
+
+
+while True:
+    
+    data = []
+
+    #Getting magnetometer readings
+    mag = hat.get_compass_raw()
+    mag_data = []
+    mag_data.append(round(mag["x"],2))
+    mag_data.append(round(mag["y"],2))
+    mag_data.append(round(mag["z"],2))
+    
+
+    #Getting accelerometer readings
+    acc = hat.get_accelerometer_raw()
+    acc_data = []
+    acc_data.append(round(acc["x"],3))
+    acc_data.append(round(acc["y"],3))
+    acc_data.append(round(acc["z"],3))
+
+    #Getting gyroscope reading
+    gyro = hat.get_orientation()
+    gyro_data = []
+    gyro_data.append(round(gyro["pitch"],2))
+    gyro_data.append(round(gyro["yaw"],2))
+    gyro_data.append(round(gyro["roll"],2))
+    
+    data.append(mag_data)
+    data.append(acc_data)
+    data.append(gyro_data)
+    
+    #Write acquired data to file
+    with open(path,'a') as file:
+        file.write(str(data)+"\n")
+    
+    #Read data every 0.5 seconds
+    sleep(0.5) 
